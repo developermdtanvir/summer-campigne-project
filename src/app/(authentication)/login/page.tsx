964433fb76f83858ce2c6078256fae5d1bd4c616
mdtanvirgit <freelencertanvirhossain@gmail.com>
2023-07-15 08:28:12 +0600

    login page working

--- src/app/(authentication)/login/page.tsx
+++ src/app/(authentication)/login/page.tsx
@@ -1,10 +1,22 @@
-
+'use client'
 const Login = () => {
+    const handleLogin = (e:any) => {
+        e.preventDefault();
+        console.log(e.target.name.value)
+    }
+
   return (
-    <div>
-        <h1>This is Login page</h1>
+    <div className="h-screen flex justify-center items-center">
+        <div className="lg:w-1/2 mx-auto">
+            <form className="form-control space-y-4" onSubmit={handleLogin} action="">
+                <input className="input" type="text" name="name" id="name" placeholder="Name" />
+                <input className="input" type="email" name="email" id="email" placeholder="Email" />
+                <input className="input" type="password" name="password" id="password" placeholder="Password" />
+                <button className="btn">Submit</button>
+            </form>
+        </div>
     </div>
   )
 }
 
-export default Login
+export default Login;
